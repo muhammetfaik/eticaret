f3074f914f76ea50173fbb514164efdb72133c0d
Muhammet Faik Alada <muhammetfaik@gmail.com>
2020-11-01 16:01:39 +0300

    indirimli cok satan v.s.

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UrunDetay;
 use Illuminate\Http\Request;
 use App\Models\Kategori;
+use App\Models\Urun;
 
 class AnasayfaController extends Controller
 {
@@ -13,8 +14,16 @@ class AnasayfaController extends Controller
         $kategoriler = Kategori::whereRaw('ust_id is null')->take(8)->get();
 
        $urunler_slider = UrunDetay::with('urun')->where('goster_slider',1)->take(5)->get();
-       $urun_gunun_firsati = Urun::select('urun_detay','urun_detay.urun_id','urun.id')-where('urun_detay.goster_gunun_firsati',1)
-               ->orderBy('guncelleme_tarihi','desc')->first();
-       return view('anasayfa',compact('kategoriler','urunler_slider'));
+       $urun_gunun_firsati = Urun::select('urun.*')
+           ->join('urun_detay','urun_detay.urun_id','urun.id')
+           ->where('urun_detay.goster_gunun_firsati',1)
+           ->orderBy('guncelleme_tarihi','desc')
+           ->first();
+        $urunler_one_cikan = UrunDetay::with('urun')->where('goster_one_cikan',1)->take(4)->get();
+        $urunler_cok_satan = UrunDetay::with('urun')->where('goster_cok_satan',1)->take(4)->get();
+        $urunler_indirimli = UrunDetay::with('urun')->where('goster_indirimli',1)->take(4)->get();
+
+
+        return view('anasayfa',compact('kategoriler','urunler_slider','urun_gunun_firsati','urunler_one_cikan','urunler_cok_satan','urunler_indirimli'));
     }
 }
--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -23,8 +23,9 @@
                 <div class="panel panel-default" id="sidebar-product">
                     <div class="panel-heading">Günün Fırsatı</div>
                     <div class="panel-body">
-                        <a href="#">
-                            <img src="http://lorempixel.com/400/485/food/1" class="img-responsive">
+                        <a href="{{  }}">
+                            <img src="http://via.placeholder.com/640x400?text=UrunResmi" class="img-responsive">
+                            {{ $urun_gunun_firsati->urun_adi }}
                         </a>
                     </div>
                 </div>
@@ -37,26 +38,13 @@
                 <div class="panel-heading">Öne Çıkan Ürünler</div>
                 <div class="panel-body">
                     <div class="row">
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/1"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/2"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/3"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                        </div>
-                        <div class="col-md-3 product">
-                            <a href="#"><img src="http://lorempixel.com/400/400/food/4"></a>
-                            <p><a href="#">Ürün adı</a></p>
-                            <p class="price">129 ₺</p>
-                        </div>
+                        @foreach($urunler_one_cikan as $urun_detay)
+                            <div class="col-md-3 product">
+                                <a href="{{ route('urun',$urun_detay->urun->slug) }}"><img src="http://via.placeholder.com/640x400?text=UrunResmi"></a>
+                                <p><a href="#">{{ $urun_detay->urun->urun_adi }}</a></p>
+                                <p class="price">{{ $urun_detay->urun->fiyati }}</p>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
